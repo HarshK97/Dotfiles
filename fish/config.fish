6b30a04f7e10c0cf9a61fdad9ea6e4c8bb859fb5
HarshK97 <harshkapse1234@gmail.com>
2025-02-28 19:35:49 +0530

    Switched to Ghostty from kitty

--- fish/config.fish
+++ fish/config.fish
@@ -50,4 +50,11 @@ end
 set -x XCURSOR_THEME rose-pine-hyprcursor
 set -x XCURSOR_SIZE 24
 
-
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
