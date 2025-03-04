8d49994a944fcabcafaedc10bdf0270d5fad923f
HarshK97 <harshkapse1234@gmail.com>
2025-03-04 15:09:09 +0530

    Documentation for copilot chat
    Added documentation comments to the key mapping setup for Copilot Chat
    in the Neovim configuration file. These comments provide clarity on the
    purpose and functionality of the code, aiding in better understanding
    and maintenance.

--- nvim/lua/harsh/plugins/CopilotChat.lua
+++ nvim/lua/harsh/plugins/CopilotChat.lua
@@ -18,12 +18,14 @@ return {
         return
       end
 
+      -- Setup CopilotChat with specified options
       copilot_chat.setup({
         debug = false, -- Enable debug logging
         notify = false,
       })
 
-      vim.keymap.set("n", "<leader>cc", function()
+      -- Keymap to prompt for Copilot Chat input
+      vim.keymap.set({ "n", "v" }, "<leader>cc", function()
         vim.ui.input({ prompt = "Copilot Chat: " }, function(prompt)
           if prompt and prompt ~= "" then
             vim.cmd("CopilotChat " .. prompt)
@@ -31,14 +33,32 @@ return {
         end)
       end, { desc = "Copilot Chat prompt" })
 
+      -- Keymap to toggle Copilot Chat
       vim.keymap.set("n", "<leader>ct", function()
         copilot_chat.toggle()
       end, { desc = "Toggle Copilot Chat" })
+
+      -- Keymap to generate commit and tests
       vim.keymap.set("n", "<leader>cg", "", { desc = "Generate Commit and Tests" })
-      vim.keymap.set("n", "<leader>ce", "<cmd>CopilotChatExplain<CR>", { desc = "Explain Selected Code" })
+
+      -- Keymap to explain selected code
+      vim.keymap.set({ "n", "v" }, "<leader>ce", "<cmd>CopilotChatExplain<CR>", { desc = "Explain Selected Code" })
+
+      -- Keymap to commit selected code
       vim.keymap.set("n", "<leader>cgc", "<cmd>CopilotChatCommit<CR>", { desc = "Commit Selected Code" })
-      vim.keymap.set("n", "<leader>cd", "<cmd>CopilotChatDocs<CR>", { desc = "Documentation for Selected Code" })
+
+      -- Keymap to generate documentation for selected code
+      vim.keymap.set(
+        { "n", "v" },
+        "<leader>cd",
+        "<cmd>CopilotChatDocs<CR>",
+        { desc = "Documentation for Selected Code" }
+      )
+
+      -- Keymap to generate tests for selected code
       vim.keymap.set("n", "<leader>cgt", "<cmd>CopilotChatTests<CR>", { desc = "Generate Tests for Selected Code" })
+
+      -- Keymap to review code
       vim.keymap.set("n", "<leader>cr", "<cmd>CopilotChatReview<CR>", { desc = "Review Code" })
     end,
   },
